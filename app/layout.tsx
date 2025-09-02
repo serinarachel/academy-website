b4c97aa441e4ed16b7bdc5d448df59aeac8087af
adsmagnify <adsmagnify@gmail.com>
2025-09-02 17:44:20 +0530

    Integrated Google tag/manager code

--- app/layout.tsx
+++ app/layout.tsx
@@ -95,8 +95,43 @@ export default function RootLayout({
             alt=""
           />
         </noscript>
+
+        {/* Google Analytics Tag */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-DRM8XJXYMZ"
+          strategy="afterInteractive"
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-DRM8XJXYMZ');
+          `}
+        </Script>
+
+        {/* Google Tag Manager (Head) */}
+        <Script id="gtm-head" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P6XXV5G9');
+          `}
+        </Script>
       </head>
       <body className="font-satoshi">
+        {/* Google Tag Manager (noscript) */}
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P6XXV5G9"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          ></iframe>
+        </noscript>
+
         <Header />
         <main>{children}</main>
         <Footer />
